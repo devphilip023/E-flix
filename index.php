1808e33dfa4d3a30dd7eb0f6103aeb18102e0619
devephilip023 <devephilip023@gmail.com>
2022-11-26 11:55:27 +0300

    #

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ include "admin/db_connection.php";
 <body>
 
   <!-- Navigation -->
-
   <?php include "layout/topnavigation.php"; ?>
+
   <!-- Page Content -->
   <div class="container">
 
@@ -31,9 +31,8 @@ include "admin/db_connection.php";
         }
         $start = $no_posts_per_page * $page - $no_posts_per_page;
         $sql_select_post = "SELECT * FROM posts WHERE post_status = 1 ORDER BY id desc LIMIT {$start} ,{$no_posts_per_page} ";
-        $result_sql_select_post = mysqli_query ($dbconnection, $sql_select_post);
-        while ($rowpost = mysqli_fetch_assoc($result_sql_select_post))
-
+        $result_sql_select_post = mysqli_query($dbconnection, $sql_select_post);
+        while ($rowpost = mysqli_fetch_assoc($result_sql_select_post)) {
           $view_post_id = $rowpost['id'];
           $view_post_category = $rowpost['post_category'];
           $view_post_title = $rowpost['post_title'];
@@ -46,8 +45,6 @@ include "admin/db_connection.php";
           $view_post_visit_counter = $rowpost['post_visit_counter'];
           $view_post_status = $rowpost['post_status'];
           $view_post_priority = $rowpost['post_priority'];
-
-          
         ?>
           <!-- Blog Post -->
           <div class="card mb-4">
