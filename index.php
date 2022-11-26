ea25ccac39ea7bb107b30aff77ee370ccbdbef50
devephilip023 <devephilip023@gmail.com>
2022-11-26 10:04:16 +0300

    #

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ include "admin/db_connection.php";
         }
         $start = $no_posts_per_page * $page - $no_posts_per_page;
         $sql_select_post = "SELECT * FROM posts WHERE post_status = 1 ORDER BY id desc LIMIT {$start} ,{$no_posts_per_page} ";
-        $result_sql_select_post = mysqli_query($dbconnection, $sql_select_post);
+        //$result_sql_select_post = mysqli_query($dbconnection, $sql_select_post);
         while ($rowpost = mysqli_fetch_assoc($result_sql_select_post)) {
           $view_post_id = $rowpost['id'];
           $view_post_category = $rowpost['post_category'];
