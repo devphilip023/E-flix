e7b84f8d644bdb61308680d83cfe50e8b69b3ea4
devephilip023 <devephilip023@gmail.com>
2022-11-28 18:07:34 +0300

    #

--- admin/layout/header.php
+++ admin/layout/header.php
@@ -36,7 +36,53 @@
         <?php //include "layout/modal/add_new_post.php"; 
         ?>
         <!-- // Modal add new Post -->
+
+        <!-- Tasks: style can be found in dropdown.less -->
         
+          </ul>
+        </li>
+        <!-- User Account: style can be found in dropdown.less -->
+        <li class="dropdown user user-menu">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <img src="images/users/<?php echo $success_login_image_admin; ?>" class="user-image" alt="User Image">
+            <span class="hidden-xs"><?php echo $success_login_name_admin; ?></span>
+          </a>
+          <ul class="dropdown-menu">
+            <!-- User image -->
+            <li class="user-header">
+              <img src="images/users/<?php echo $success_login_image_admin; ?>" class="zoom3" alt="User Image">
+
+              <p>
+                <?php echo $success_login_name_admin; ?> - Writing posts
+                <small>Since Nov. 2022</small>
+              </p>
+            </li>
+            <!-- Menu Body -->
+            <li class="user-body">
+              <div class="row">
+                <div class="col-xs-4 text-center">
+                  <a href="#">Followers</a>
+                </div>
+                <div class="col-xs-4 text-center">
+                  <a href="#">Sales</a>
+                </div>
+                <div class="col-xs-4 text-center">
+                  <a href="#">Friends</a>
+                </div>
+              </div>
+              <!-- /.row -->
+            </li>
+            <!-- Menu Footer-->
+            <li class="user-footer">
+              <div class="pull-left">
+                <button type="button" name="edit" class="btn btn-default btn-flat" data-toggle="modal" data-target="#EditUser" data-user_id_edit="<?= $success_login_id ?>" data-user_name_edit="<?= $success_login_name_admin ?>" data-user_username_edit="<?= $success_login_username_admin ?>" data-user_email_edit="<?= $success_login_email_admin ?>" data-user_image_edit="<?= $success_login_image_admin ?>" data-user_type_edit="<?= $success_login_type_admin ?>" data-user_type_edit1="<?= $success_login_type_admin ?>" data-user_gender_edit="<?= $success_login_gender_admin ?>" data-user_password_edit="<?= $success_login_type_password_admin ?>"> <span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Profile</button>
+              </div>
+              <div class="pull-right">
+                <a href="../layout/logout.php" class="btn btn-default btn-flat">Sign out</a>
+              </div>
+            </li>
+          </ul>
+        </li>
         <!-- Control Sidebar Toggle Button -->
         <li>
           <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
