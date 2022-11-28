aa6a7c80cbdeca8337473d11f22f863289268b26
devephilip023 <devephilip023@gmail.com>
2022-11-28 12:51:32 +0300

    #

--- index.php
+++ index.php
@@ -10,14 +10,11 @@ include "admin/db_connection.php";
   <style>
     .video-responsive {
       overflow: hidden;
-      padding-bottom: 56.25%;
       position: relative;
       height: 0;
     }
 
     .video-responsive iframe {
-      left: 0;
-      top: 0;
       height: 100%;
       width: 100%;
       position: absolute;
